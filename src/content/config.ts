d9cf10d0fe7bdb5b78f954c5f748ead4262b7c21
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-07-22 22:03:34 -0500

    26 - Referencing other collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const team = defineCollection({
   type: "data",
@@ -9,6 +9,21 @@ const team = defineCollection({
   }),
 });
 
+
+
+const posts = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string().max(65, { message: "Must be 65 characters or less" }),
+    tags: z.array(z.string()),
+    pubDate: z.string(),
+    isDraft: z.boolean(),
+    canonicalURL: z.string().url().optional(),
+    author:reference("team"),
+  }),
+});
+
 export const collections = {
   team,
+  posts,
 };
---
+++ src/content/posts/post-1.md
@@ -0,0 +1,9 @@
+---
+title: Post 1
+tags: ["product","tutorial"]
+pubDate: "2022-01-01"
+isDraft: false
+canonicalURL: "https://example.com"
+author: christina
+---
+
